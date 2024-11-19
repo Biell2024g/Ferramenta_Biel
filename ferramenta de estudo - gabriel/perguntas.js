5347fec4c4f057065cb152b191f281e3df47f68a
Biell2024g <0000110402052xsp@al.educacao.sp.gov.br>
2024-11-18 22:17:56 -0300

    Add files via upload

---
+++ ferramenta de estudo - gabriel/perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'matematica',
+    ' 1º algarismo é o dobro do segundo e a soma dos dois tem como resultado o 3º algarismo, que é',
+    'Seja divisível por 5; O 1º algarismo é o dobro do segundo e a soma dos dois tem como resultado o 3º algarismo, que é 6; O penúltimo algarismo é um número que multiplicado por ele mesmo tem como resultado 9; O 4º algarismo corresponde a soma do 2º e 6º algarismo.'
+)
+
+criaCartao(
+    '',
+    'Adição, subtração, multiplicação e divisão',
+    'O resultado de uma adição é a soma, e o resultado de uma subtração é a diferença. O resultado de uma multiplicação é o produto, e o resultado de uma divisão é o quociente.
+'
+)
+
+criaCartao(
+    '',
+    '',O 1º algarismo é o dobro do segundo e a soma dos dois tem como resultado o 3º algarismo, que é 6;
+    'estamos a referir-nos ao símbolo, devemos dizer o algarismo'
+)
+
+criaCartao(
+    'o que é multiplicação',
+    'podemos escrever 2 + 2 + 2 como 3 × 2.
+',
+    ''
+)
